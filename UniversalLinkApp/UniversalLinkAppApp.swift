bc42120f9e9c479ce37b0e5cb8db7cceebabc562
Goh Chong Yong <gcyong2004@gmail.com>
2024-01-04 16:01:49 +0800

    Initial Commit

---
+++ UniversalLinkApp/UniversalLinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalLinkAppApp.swift
+//  UniversalLinkApp
+//
+//  Created by Goh Chong Yong on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalLinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
